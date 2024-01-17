a38616e8b941009806317245704c0047653a3617
Rafel Amengual Tomás <147522281+RafelAm@users.noreply.github.com>
2024-01-17 10:15:12 +0100

    Create Five.java

---
+++ Five.java
@@ -0,0 +1,11 @@
+        int start = 1;
+        int end = 1000;
+          
+        for (int i = start; i <= end; i++) {
+            String num= i+"";
+            if (num.contains("5")) {
+                
+            }else{
+                System.out.println(i);
+            }
+        }
