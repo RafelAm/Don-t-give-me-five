951a46e119be71c4e348ef28ef55f0fb0e8da294
Rafel Amengual Tomás <147522281+RafelAm@users.noreply.github.com>
2024-01-18 17:10:07 +0100

    Update Five.java

--- Five.java
+++ Five.java
@@ -1,11 +1,20 @@
+
+          
+public class Kata
+{
+  public static int dontGiveMeFive(int start, int end)
+  {
         int start = 1;
         int end = 1000;
-          
+        int num1 = 0;
+    
         for (int i = start; i <= end; i++) {
             String num= i+"";
-            if (num.contains("5")) {
-                
-            }else{
-                System.out.println(i);
+            if (!num.contains("5")) {
+                num1 ++;
             }
         }
+    
+    return num1;
+  }
+}
